4cb2332555c72d7c8b926c5cc404f888035cd28d
Birgit Hanniste <birgit.hanniste@tptlive.ee>
2024-10-21 10:42:23 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+const __filename = import.meta.filename
+const __dirname = import.meta.dirname
+
+export default{
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+}
