b279f4160595dd26c73313e505a04f9dadc72c62
Birgit Hanniste <birgit.hanniste@tptlive.ee>
2024-11-04 11:40:49 +0200

    add bulma

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,63 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 
 const __filename = import.meta.filename
 const __dirname = import.meta.dirname
 
-export default{
+export default {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+                "style-loader", 
+                "css-loader", 
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sassOptions: {
+                            quietDeps: true
+                        }
+                    } 
+                  }
+                ],
+         
+          },
+          {
+            test: /\.njk$/,
+            use: [
+                {
+                    loader: 'simple-nunjucks-loader',
+                    options: {}
+                }
+            ]
+        }
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'about.html'
         }),
     ],
 }
